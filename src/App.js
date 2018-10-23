1bc0c69dc9d1bcf9be87bf77d989e0f179a56708
Cristiano S. Oliveira <cristiano.oliveira@venturus.org.br>
2018-10-23 00:14:58 -0300

    App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// @flow
+import React, { Component, Fragment } from 'react'
+import { hot } from 'react-hot-loader'
+
+class App extends Component<{}, { text: string }> {
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App)

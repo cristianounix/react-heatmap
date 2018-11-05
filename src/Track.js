b771e615f643450831dcead52c2c02ee49300187
Cristiano S. Oliveira <cristianounix@gmail.com>
2018-11-05 12:53:53 -0200

    Track component

--- src/Track.js
+++ src/Track.js
@@ -5,25 +5,33 @@ class Track extends React.Component {
   constructor() {
     super();
     this.state = {
-      store:[],
+      store: {},
     };
 
     this.handleMousemove = this.handleMousemove.bind(this);
     this.throttle = this.throttle.bind(this);
+    this.increasePositionIfhas = this.increasePositionIfhas.bind(this);
 
   }
   
-  findAndIncreasePosition({x = 0, y = 0 }) {
-    const index = this.state.store.findIndex(p => p.x == x && p.y == y);
+  increasePositionIfhas({x = 0, y = 0, w = 0 }) {
+    const key = `${x}-${y}`;
+    const state = Object.assign({}, this.state);
 
-    // Use state.store as a big object where the keys will be position, like it, key = x-y 
+    state.store[key] = {
+      x: x,
+      y: y,
+      w: w,
+    };
 
-    // this.state.store.
+    if (state.store[key] && state.store[key].x == x && state.store[key].y == y ) {
+      state.store[key].w = state.store[key].w + 1;
+    }
 
-    //   if (p.x == x && ) {
-
-    //   }
-    // });
+    console.log(`X:${state.store[key].x} - Y:${state.store[key].y} - W:${state.store[key].w}`);
+    this.setState(() => {
+      state
+    });
   }
 
   handleMousemove(event) {
@@ -39,11 +47,7 @@ class Track extends React.Component {
       y: event.y,
       w: 1,
     };
-    if (obj.x == obj.y) {
-      obj.w += 1;
-    }
-    this.state.store.push(obj);
-    console.log(`X ${obj.x} - Y:${obj.y} - W:${obj.w}`);
+    this.increasePositionIfhas(obj);
   }
   
   throttle(func, delay) {
@@ -59,7 +63,7 @@ class Track extends React.Component {
   
   componentDidMount() {
     console.log('ssStore: ', this.state.store);
-    window.document.addEventListener('mousemove', this.throttle(this.handleMousemove, 1000));
+    window.document.addEventListener('mousemove', this.throttle(this.handleMousemove, 300));
   }
   
   render() {

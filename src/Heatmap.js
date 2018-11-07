6d3d302f39a056008ec077aff796916e1b74fe7d
Cristiano S. Oliveira <cristianounix@gmail.com>
2018-11-07 15:17:42 -0200

    Get data from tracker

--- src/Heatmap.js
+++ src/Heatmap.js
@@ -6,10 +6,11 @@ class Heatmap extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      draw: simpleheat,
-    };
+    this.state = {};
 
+    this.simpleheat = require('./DrawMap');
+
+    this.canvas = React.createRef();
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -20,7 +21,18 @@ class Heatmap extends React.Component {
     return props.data;
   }
 
+  componentDidUpdate() {
+    console.log('UPDATE');
+    this.simpleheat.default(this.canvas.current).data(
+      { x: 234, y: 354, w: 1},
+      { x: 453, y: 62, w: 1},
+      { x: 634, y: 346, w: 1},
+    ).max(18)
+    // simpleheat('canvas').data(data).max(18)
+  }
+
   componentDidMount() {
+    // this.contextCanvas = this.canvas.current.getContext('2d');
     // console.log('DID Mount: ', this.props);
     // create refs for canvas
     // heat = simpleheat('canvas').data(data).max(18)
@@ -29,7 +41,7 @@ class Heatmap extends React.Component {
   render() {
     return (
       <div>
-        <div id="canvas"></div>
+        <canvas id="canvas" width="1000" height="600" ref={this.canvas}></canvas>
       </div>
     );
   }

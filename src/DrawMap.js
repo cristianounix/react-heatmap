f87ec2dcdbf25493156b76cd0dab380dd07d60da
Cristiano S. Oliveira <cristianounix@gmail.com>
2018-11-28 22:30:21 -0200

    Component Map

--- src/DrawMap.js
+++ src/DrawMap.js
@@ -1,143 +1,84 @@
-// import React from 'react';
-// 'use strict';
-// // https://github.com/mourner/simpleheat
-// if (typeof module !== 'undefined') module.exports = simpleheat;
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import * as d3 from "d3";
 
-simpleheat.prototype = {
+class DrawMap extends Component {
+  constructor(props) {
+    super(props);
+    this.displayName = "DrawMap";
+  }
+  componentDidMount() {
+    this.drawArc();
+  }
 
-    defaultRadius: 25,
+  componentDidUpdate() {
+    this.redrawArc();
+  }
 
-    defaultGradient: {
-        0.4: 'blue',
-        0.6: 'cyan',
-        0.7: 'lime',
-        0.8: 'yellow',
-        1.0: 'red'
-    },
+  drawArc() {
+    const context = this.setContext();
+    this.setBackground(context);
+    this.setForeground(context);
+  }
 
-    data: function (data) {
-        this._data = data;
-        return this;
-    },
+  redrawArc() {
+    const context = d3.select("#d3-arc");
+    context.remove();
+    this.drawArc();
+  }
 
-    max: function (max) {
-        this._max = max;
-        return this;
-    },
+  setContext() {
+    const { height, width, id } = this.props;
+    return d3
+      .select(this.refs.arc)
+      .append("svg")
+      .attr("height", height)
+      .attr("width", width)
+      .attr("id", id)
+      .append("g")
+      .attr("transform", `translate(${height / 2}, ${width / 2})`);
+  }
 
-    add: function (point) {
-        this._data.push(point);
-        return this;
-    },
+  setBackground(context) {
+    return context
+      .append("path")
+      .datum({ endAngle: this.tau })
+      .style("fill", this.props.backgroundColor)
+      .attr("d", this.arc());
+  }
 
-    clear: function () {
-        this._data = [];
-        return this;
-    },
+  setForeground(context) {
+    return context
+      .append("path")
+      .datum({ endAngle: this.tau * this.props.percentComplete })
+      .style("fill", this.props.foregroundColor)
+      .attr("d", this.arc());
+  }
 
-    radius: function (r, blur) {
-        blur = blur === undefined ? 15 : blur;
+  tau = Math.PI * 2;
 
-        // create a grayscale blurred circle image that we'll use for drawing points
-        var circle = this._circle = this._createCanvas(),
-            ctx = circle.getContext('2d'),
-            r2 = this._r = r + blur;
+  arc() {
+    return d3
+      .arc()
+      .innerRadius(this.props.innerRadius)
+      .outerRadius(this.props.outerRadius)
+      .startAngle(0);
+  }
 
-        circle.width = circle.height = r2 * 2;
+  render() {
+    return <div ref="arc" />;
+  }
+}
 
-        ctx.shadowOffsetX = ctx.shadowOffsetY = r2 * 2;
-        ctx.shadowBlur = blur;
-        ctx.shadowColor = 'black';
-
-        ctx.beginPath();
-        ctx.arc(-r2, -r2, r, 0, Math.PI * 2, true);
-        ctx.closePath();
-        ctx.fill();
-
-        return this;
-    },
-
-    resize: function () {
-        this._width = this._canvas.width;
-        this._height = this._canvas.height;
-    },
-
-    gradient: function (grad) {
-        // create a 256x1 gradient that we'll use to turn a grayscale heatmap into a colored one
-        var canvas = this._createCanvas(),
-            ctx = canvas.getContext('2d'),
-            gradient = ctx.createLinearGradient(0, 0, 0, 256);
-
-        canvas.width = 1;
-        canvas.height = 256;
-
-        for (var i in grad) {
-            gradient.addColorStop(+i, grad[i]);
-        }
-
-        ctx.fillStyle = gradient;
-        ctx.fillRect(0, 0, 1, 256);
-
-        this._grad = ctx.getImageData(0, 0, 1, 256).data;
-
-        return this;
-    },
-
-    draw: function (minOpacity) {
-        if (!this._circle) this.radius(this.defaultRadius);
-        if (!this._grad) this.gradient(this.defaultGradient);
-
-        var ctx = this._ctx;
-
-        ctx.clearRect(0, 0, this._width, this._height);
-
-        // draw a grayscale heatmap by putting a blurred circle at each data point
-        for (var i = 0, len = this._data.length, p; i < len; i++) {
-            p = this._data[i];
-            ctx.globalAlpha = Math.min(Math.max(p[2] / this._max, minOpacity === undefined ? 0.05 : minOpacity), 1);
-            ctx.drawImage(this._circle, p[0] - this._r, p[1] - this._r);
-        }
-
-        // colorize the heatmap, using opacity value of each pixel to get the right color from our gradient
-        var colored = ctx.getImageData(0, 0, this._width, this._height);
-        this._colorize(colored.data, this._grad);
-        ctx.putImageData(colored, 0, 0);
-
-        return this;
-    },
-
-    _colorize: function (pixels, gradient) {
-        for (var i = 0, len = pixels.length, j; i < len; i += 4) {
-            j = pixels[i + 3] * 4; // get gradient color from opacity value
-
-            if (j) {
-                pixels[i] = gradient[j];
-                pixels[i + 1] = gradient[j + 1];
-                pixels[i + 2] = gradient[j + 2];
-            }
-        }
-    },
-
-    _createCanvas: function () {
-        if (typeof document !== 'undefined') {
-            return document.createElement('canvas');
-        } else {
-            // create a new canvas instance in node.js
-            // the canvas class needs to have a default constructor without any parameter
-            return new this._canvas.constructor();
-        }
-    }
+ProgressArc.propTypes = {
+  id: PropTypes.string,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  innerRadius: PropTypes.number,
+  outerRadius: PropTypes.number,
+  backgroundColor: PropTypes.string,
+  foregroundColor: PropTypes.string,
+  percentComplete: PropTypes.number
 };
 
-export default function simpleheat(canvas) {
-    if (!(this instanceof simpleheat)) return new simpleheat(canvas);
-
-    this._canvas = canvas = typeof canvas === 'string' ? document.getElementById(canvas) : canvas;
-
-    this._ctx = canvas.getContext('2d');
-    this._width = canvas.width;
-    this._height = canvas.height;
-
-    this._max = 1;
-    this._data = [];
-}
+export default DrawMap;
